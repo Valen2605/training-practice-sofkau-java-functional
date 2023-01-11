5b6ab98c8e97ecf8f6b715fbb9e5c7d9ea8557f8
Valentina Santa Muñoz <valentinas792 @gmail.com>
2023-01-11 17:01:48 -0500

    Kata3
    Se usa map y FlatMap para retornar una lista de enteros de las peliculas

--- src/main/java/katas/Kata3.java
+++ src/main/java/katas/Kata3.java
@@ -6,6 +6,7 @@ import model.MovieList;
 import util.DataUtil;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
     Goal: Use map() and flatMap() to project and flatten the movieLists into an array of video ids (flatMap(c -> c.stream()))
@@ -13,9 +14,21 @@ import java.util.List;
     Output: List of Integers
 */
 public class Kata3 {
+//    lista.stream().map(persona - > persona.getLista())
+//            .flatMap(viajes -> viajes.stream())
+//            .forEach(new Consumer < Viaje > () {
+//        @Override
+//        public void accept(Viaje t) {
+//            System.out.println(t.getPais());
+//        }
+//    });
+
     public static List<Integer> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(1, 2, 3);
+        return movieLists.stream()
+                .flatMap(movieList -> movieList.getVideos().stream())
+                .map(Movie::getId)
+                .collect(Collectors.toList());
     }
 }
